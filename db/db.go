94a0a7bebadd11cfe914d1745df89a773e9d2023
Muhammad Faiz Asy Syauqi <m.faizasysyauqi@gmail.com>
2023-09-04 16:42:00 +0700

    add inventory book

---
+++ db/db.go
@@ -0,0 +1,67 @@
+package db
+
+import (
+	"log"
+	"main/models"
+	"os"
+
+	_ "database/sql"
+
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func InitDB() *gorm.DB {
+	err := godotenv.Load("env.env")
+	if err != nil {
+		log.Fatal("Error load env")
+	}
+
+	conn := os.Getenv("POSTGRES_URL")
+	db, err := gorm.Open("postgres", conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Migrate(db)
+
+	return db
+}
+
+func Migrate(db *gorm.DB) {
+	db.AutoMigrate(&models.Books{})
+
+	data := models.Books{}
+	if db.Find(&data).RecordNotFound() {
+		seederBook(db)
+	}
+}
+
+func seederBook(db *gorm.DB) {
+	data := []models.Books{{
+		Title:       "Perjalanan Ini",
+		Author:      "Jojo",
+		Description: "Buku tentang perjalanan",
+		Stock:       10,
+	}, {
+		Title:       "Pengobatan Herbal",
+		Author:      "Sindy",
+		Description: "Buku tentang pengobatan",
+		Stock:       5,
+	}, {
+		Title:       "Seputar Hewan",
+		Author:      "Kiki",
+		Description: "Buku tentang hewan",
+		Stock:       7,
+	}, {
+		Title:       "Berita terkini",
+		Author:      "Sindy",
+		Description: "Buku tentang berita",
+		Stock:       5,
+	}}
+
+	for _, v := range data {
+		db.Create(&v)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module main
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.8.1
+	github.com/golang-jwt/jwt/v4 v4.4.2
+	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.4.0
+	github.com/lib/pq v1.10.7
+)
